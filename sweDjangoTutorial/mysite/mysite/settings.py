813569569c8e10c0aff8fac1d457321dc69e2f78
Aaqil Khoja <aaqilkhoja@gmail.com>
2023-03-05 22:37:54 -0500

    pushed

--- sweDjangoTutorial/mysite/mysite/settings.py
+++ sweDjangoTutorial/mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-k=5fvi_3hrb8)hcr^5r$0$7ik!47o6j(h7(y@z_a-1450gougg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["sweapp-env.eba-33kex9d3.us-west-2.elasticbeanstalk.com"]
+ALLOWED_HOSTS = ["assignment2-env.eba-ikmqf6rf.us-west-2.elasticbeanstalk.com","127.0.0.1"]
 
 
 # Application definition
